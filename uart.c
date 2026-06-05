fb7bcd2e0817c27232a9953c0877a65dfe5f6fff
Lev Alyukov <lev.alyukov@gmail.com>
2026-06-05 07:58:23 +0500

    add uart (in development)

---
+++ uart.c
@@ -0,0 +1,68 @@
+#include "uart.h"
+
+error_t uart_init(const uart_parity_control_t parity)
+{
+    check(UART_PARITY_IS_OK(parity), INVALID_ARG);
+
+    UART->BRR1 = (unsigned char)0x41;
+    UART->BRR2 = (unsigned char)0x02;
+
+    return OK;
+}
+
+error_t uart_enable(void)
+{
+    return OK;
+}
+
+error_t uart_disable(void)
+{
+    return OK;
+}
+
+error_t uart_send(const unsigned char data, const uart_transmit_t type)
+{
+    check(!(UART->CR1 & (1 << 5)), FAIL);
+    check(UART_TRANSMIT_IS_OK(type), INVALID_ARG);
+
+    if (type == UART_DATA_TRANSMITTER)
+    {
+        if (UART->SR & (1 << 7))
+            return FAIL;
+
+        UART->CR2 |= (1 << 3);
+    }
+
+    if (type == UART_DATA_RECEIVER)
+    {
+        if (UART->SR & (1 << 5))
+            return FAIL;
+
+        UART->CR2 |= (1 << 2);
+    }
+
+    UART->DR = data;
+
+    return OK;
+}
+
+unsigned char uart_read(void)
+{
+    check(!(UART->CR1 & (1 << 5)), 0x00);
+    return UART->DR;
+}
+
+void uart_deinit(void)
+{
+    UART->SR = (unsigned char)0xC0;
+    UART->DR = (unsigned char)0x00; /* in datasheet written 0xXX */
+    UART->BRR1 = (unsigned char)0x00;
+    UART->BRR2 = (unsigned char)0x00;
+    UART->CR1 = (unsigned char)0x00;
+    UART->CR2 = (unsigned char)0x00;
+    UART->CR3 = (unsigned char)0x00;
+    UART->CR4 = (unsigned char)0x00;
+    UART->CR5 = (unsigned char)0x00;
+    UART->GTR = (unsigned char)0x00;
+    UART->PSCR = (unsigned char)0x00;
+}
--- uart.h
+++ uart.h
@@ -1,14 +1,21 @@
 #ifndef UART_H
 #define UART_H
 
+#include "error.h"
+
 #define UART ((uart_t *)0x005230)
+#define UART_TRANSMIT_IS_OK(type)                                         \
+    (((type) == UART_DATA_TRANSMITTER) ||                                 \
+     ((type) == UART_DATA_RECEIVER) || ((type) == UART_DATA_DUPLEX))
+#define UART_PARITY_IS_OK(parity)                                         \
+    (((parity) == UART_PARITY_DISABLE) || ((parity) == UART_PARITY_ENABLE))
 
 typedef struct uart
 {
     volatile unsigned char SR;   /* Status Register */
     volatile unsigned char DR;   /* Data Register */
     volatile unsigned char BRR1; /* Baud Rate Register 1 */
-    volatile unsigned char BBR2; /* Baud Rate Register 2 */
+    volatile unsigned char BRR2; /* Baud Rate Register 2 */
     volatile unsigned char CR1;  /* Control Register 1 */
     volatile unsigned char CR2;  /* Control Register 2 */
     volatile unsigned char CR3;  /* Control Register 3 */
@@ -16,6 +23,26 @@ typedef struct uart
     volatile unsigned char CR5;  /* Control Register 5 */
     volatile unsigned char GTR;  /* Guard Time Register */
     volatile unsigned char PSCR; /* Prescaler Register */
-} uart_t
+} uart_t;
+
+typedef enum uart_transmit
+{
+    UART_DATA_TRANSMITTER,
+    UART_DATA_RECEIVER,
+    UART_DATA_DUPLEX
+} uart_transmit_t;
+
+typedef enum uart_parity_control
+{
+    UART_PARITY_DISABLE = (unsigned char)0x00,
+    UART_PARITY_ENABLE = (unsigned char)0x02
+} uart_parity_control_t;
+
+error_t uart_init(const uart_parity_control_t parity);
+error_t uart_enable(void);
+error_t uart_send(unsigned char data, uart_transmit_t type);
+unsigned char uart_read(void);
+error_t uart_disable(void);
+void uart_deinit(void);
 
 #endif /* UART_H */
