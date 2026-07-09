e9eabacfedf68ad7e202e5109937c44adc6a2ebe
Lev Alyukov <lev.alyukov@gmail.com>
2026-07-09 17:33:14 +0500

    fix misra c2012 11.9 rule

--- mcu_pins_layout.h
+++ mcu_pins_layout.h
@@ -2,12 +2,12 @@
 #define MCU_PINS_H
 
 /* cppcheck-suppress-begin misra-c2012-11.4 */
-#define GPIO_A (volatile gpio_t *)0x005000U
-#define GPIO_B (volatile gpio_t *)0x005005U
-#define GPIO_C (volatile gpio_t *)0x00500AU
-#define GPIO_D (volatile gpio_t *)0x00500FU
-#define GPIO_E (volatile gpio_t *)0x005014U
-#define GPIO_F (volatile gpio_t *)0x005019U
+#define GPIO_A ((volatile gpio_t *)0x005000)
+#define GPIO_B ((volatile gpio_t *)0x005005)
+#define GPIO_C ((volatile gpio_t *)0x00500A)
+#define GPIO_D ((volatile gpio_t *)0x00500F)
+#define GPIO_E ((volatile gpio_t *)0x005014)
+#define GPIO_F ((volatile gpio_t *)0x005019)
 /* cppcheck-suppress-end misra-c2012-11.4 */
 
 typedef struct
